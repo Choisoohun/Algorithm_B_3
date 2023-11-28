2ed4aef9c7ac32e5a9b202f273202004025e3fde
Gyeongsu Kim <rudtn021371@gmail.com>
2023-11-28 10:53:04 +0900

    Update quick_sort_test.c
    원하는 조건에 맞게 정렬

--- quick_sort_test.c
+++ quick_sort_test.c
@@ -4,22 +4,71 @@
 
 struct Parcel {
     char code[15];
+    int year;
+    int date;
+    int departureRegion;
+    int arrivalRegion;
+    char deliveryStatus[3];
 };
 
-// Forward declaration
-int partition(struct Parcel arr[], int low, int high);
+int extractYear(char *code) {
+    char yearPart[5];
+    strncpy(yearPart, code, 4);
+    yearPart[4] = '\0';
+    return atoi(yearPart);
+}
 
-// 문자열에서 날짜를 추출하여 정수로 반환
-int extractDate(char *str) {
-    char datePart[9];
-    strncpy(datePart, str, 8);
-    datePart[8] = '\0'; // 문자열 마지막에 널 문자 추가
+int extractDate(char *code) {
+    char datePart[3];
+    strncpy(datePart, code + 4, 2);
+    datePart[2] = '\0';
     return atoi(datePart);
 }
 
-void quickSort(struct Parcel arr[], int low, int high);
+int extractDepartureRegion(char *code) {
+    char departurePart[3];
+    strncpy(departurePart, code + 6, 3);
+    departurePart[3] = '\0';
+    return atoi(departurePart);
+}
 
-int partition(struct Parcel arr[], int low, int high);
+int extractArrivalRegion(char *code) {
+    char arrivalPart[3];
+    strncpy(arrivalPart, code + 9, 3);
+    arrivalPart[3] = '\0';
+    return atoi(arrivalPart);
+}
+
+// 비교 함수 포인터 타입 정의
+typedef int (*CompareFunction)(struct Parcel, struct Parcel);
+
+int compareByYear(struct Parcel a, struct Parcel b) {
+    return a.year - b.year;
+}
+
+int compareByDate(struct Parcel a, struct Parcel b) {
+    return a.date - b.date;
+}
+
+int compareByDepartureRegion(struct Parcel a, struct Parcel b) {
+    return a.departureRegion - b.departureRegion;
+}
+
+int compareByArrivalRegion(struct Parcel a, struct Parcel b) {
+    return a.arrivalRegion - b.arrivalRegion;
+}
+
+// 정렬 함수
+void sortParcels(struct Parcel parcels[], int count, CompareFunction compareFunction) {
+    quickSort(parcels, 0, count - 1, compareFunction);
+}
+
+// 정렬된 데이터 출력 함수
+void printSortedParcels(struct Parcel parcels[], int count) {
+    for (int i = 0; i < count; i++) {
+        printf("%s\n", parcels[i].code);
+    }
+}
 
 int main() {
     FILE *file = fopen("Delivery_list.csv", "r");
@@ -28,66 +77,60 @@ int main() {
         return 1;
     }
 
-    struct Parcel parcels[50]; // 택배 리스트를 저장할 배열
+    // 정적 배열을 사용하여 데이터 저장
+    struct Parcel parcels[50];
     int count = 0;
 
-    // 파일에서 데이터 읽어오기
-    while (fscanf(file, "%s", parcels[count].code) == 1 && count < 50) {
+    // 파일에서 최대 50개의 데이터 읽어오기
+    while (count < 50 && fscanf(file, "%s", parcels[count].code) == 1) {
+        // 데이터를 파싱하여 구조체 멤버에 저장
+        parcels[count].year = extractYear(parcels[count].code);
+        parcels[count].date = extractDate(parcels[count].code);
+        parcels[count].departureRegion = extractDepartureRegion(parcels[count].code);
+        parcels[count].arrivalRegion = extractArrivalRegion(parcels[count].code);
+
         count++;
     }
 
     fclose(file);
 
-    // 퀵 정렬 수행
-    quickSort(parcels, 0, count - 1);
+    // 사용자로부터 정렬 기준 입력 받기
+    printf("Choose sorting criteria:\n");
+    printf("1. Year\n");
+    printf("2. Date\n");
+    printf("3. Departure Region\n");
+    printf("4. Arrival Region\n");
 
-    // 정렬된 데이터를 새로운 CSV 파일에 쓰기
-    FILE *outputFile = fopen("sorted_parcels.csv", "w");
-    if (outputFile == NULL) {
-        perror("Error opening output file");
-        return 1;
+    int choice;
+    scanf("%d", &choice);
+
+    CompareFunction compareFunction;
+
+    // 선택에 따라 비교 함수 지정
+    switch (choice) {
+        case 1:
+            compareFunction = compareByYear;
+            break;
+        case 2:
+            compareFunction = compareByDate;
+            break;
+        case 3:
+            compareFunction = compareByDepartureRegion;
+            break;
+        case 4:
+            compareFunction = compareByArrivalRegion;
+            break;
+        default:
+            printf("Invalid choice\n");
+            return 1;
     }
 
-    for (int i = 0; i < count; i++) {
-        fprintf(outputFile, "%s\n", parcels[i].code);
-    }
+    // 정렬
+    sortParcels(parcels, count, compareFunction);
 
-    fclose(outputFile);
-
-    // 정렬된 결과 출력
-    for (int i = 0; i < count; i++) {
-        printf("%s\n", parcels[i].code);
-    }
+    // 정렬된 데이터 출력
+    printf("\nSorted Parcels:\n");
+    printSortedParcels(parcels, count);
 
     return 0;
-}
-
-void quickSort(struct Parcel arr[], int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
-
-int partition(struct Parcel arr[], int low, int high) {
-    struct Parcel pivot = arr[high];
-    int i = low - 1;
-
-    for (int j = low; j < high; j++) {
-        // 수정된 부분: 문자열로 된 날짜를 정수로 변환하여 비교
-        if (extractDate(arr[j].code) < extractDate(pivot.code)) {
-            i++;
-            struct Parcel temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
-    }
-
-    struct Parcel temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
-
-    return i + 1;
 }
