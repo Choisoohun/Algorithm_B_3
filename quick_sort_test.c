36800fa5f39fd429e4d276632ef14fc3acf2ec8c
Gyeongsu Kim <rudtn021371@gmail.com>
2023-11-29 19:03:01 +0900

    Update quick_sort_test.c

--- quick_sort_test.c
+++ quick_sort_test.c
@@ -1,154 +1,166 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#define MAX_LINE_SIZE 100
+#define REGION_CODE_SIZE 3
+#define MAX_PARCELS 50
 
 struct Parcel {
-    char code[15];
-    int year;
-    int date;
-    int departureRegion;
-    int arrivalRegion;
-    char deliveryStatus[3];
+    char code[MAX_LINE_SIZE];
+    char date[9];
+    char regionNumbers[REGION_CODE_SIZE * 2 + 1];
+    char status[MAX_LINE_SIZE];
 };
 
-// 비교 함수 포인터 타입 정의
-typedef int (*CompareFunction)(struct Parcel, struct Parcel);
+// 알파벳이 나오기 전과 후를 분리하는 함수 split
+void split(const char *input, char *beforeAlpha, char *afterAlpha) {
+    int i, len = strlen(input);
 
-int extractYear(char *code);
-int extractDate(char *code);
-int extractDepartureRegion(char *code);
-int extractArrivalRegion(char *code);
+    // 초기화
+    memset(beforeAlpha, 0, len);
+    memset(afterAlpha, 0, len);
 
-int compareByYear(struct Parcel a, struct Parcel b);
-int compareByDate(struct Parcel a, struct Parcel b);
-int compareByDepartureRegion(struct Parcel a, struct Parcel b);
-int compareByArrivalRegion(struct Parcel a, struct Parcel b);
+    // 알파벳이 처음 나오는 위치 찾기
+    for (i = 0; i < len; i++) {
+        if (isalpha(input[i])) {
+            break;
+        }
+    }
 
-void quickSort(struct Parcel arr[], int low, int high, CompareFunction compareFunction);
-int partition(struct Parcel arr[], int low, int high, CompareFunction compareFunction);
+    // 알파벳이 나오기 전과 나온 후로 분리
+    strncpy(beforeAlpha, input, i);
+    strcpy(afterAlpha, input + i);
+}
 
-void sortParcels(struct Parcel parcels[], int count, CompareFunction compareFunction);
-void printSortedParcels(struct Parcel parcels[], int count);
+// 숫자를 문자열로 변환하는 함수
+const char *convertNumberToString(const char *number) {
+    static char result[MAX_LINE_SIZE];
+
+    // 숫자에 따른 지역 이름 매핑
+    if (strncmp(number, "02", REGION_CODE_SIZE) == 0) {
+        strcpy(result, "서울");
+    } else if (strncmp(number, "051", REGION_CODE_SIZE) == 0) {
+        strcpy(result, "부산");
+    } else if (strncmp(number, "053", REGION_CODE_SIZE) == 0) {
+        strcpy(result, "대구");
+    } else if (strncmp(number, "032", REGION_CODE_SIZE) == 0) {
+        strcpy(result, "인천");
+    } else if (strncmp(number, "062", REGION_CODE_SIZE) == 0) {
+        strcpy(result, "광주");
+    } else if (strncmp(number, "042", REGION_CODE_SIZE) == 0) {
+        strcpy(result, "대전");
+    } else if (strncmp(number, "052", REGION_CODE_SIZE) == 0) {
+        strcpy(result, "울산");
+    } else if (strncmp(number, "044", REGION_CODE_SIZE) == 0) {
+        strcpy(result, "세종");
+    } else if (strncmp(number, "031", REGION_CODE_SIZE) == 0) {
+        strcpy(result, "경기");
+    } else if (strncmp(number, "033", REGION_CODE_SIZE) == 0) {
+        strcpy(result, "강원");
+    } else if (strncmp(number, "043", REGION_CODE_SIZE) == 0) {
+        strcpy(result, "충북");
+    } else if (strncmp(number, "041", REGION_CODE_SIZE) == 0) {
+        strcpy(result, "충남");
+    } else if (strncmp(number, "063", REGION_CODE_SIZE) == 0) {
+        strcpy(result, "전북");
+    } else if (strncmp(number, "061", REGION_CODE_SIZE) == 0) {
+        strcpy(result, "전남");
+    } else if (strncmp(number, "054", REGION_CODE_SIZE) == 0) {
+        strcpy(result, "경북");
+    } else if (strncmp(number, "055", REGION_CODE_SIZE) == 0) {
+        strcpy(result, "경남");
+    } else if (strncmp(number, "064", REGION_CODE_SIZE) == 0) {
+        strcpy(result, "제주");
+    } else {
+        strcpy(result, "알 수 없음");
+    }
+
+    return result;
+}
+
+// 퀵 정렬을 위한 비교 함수
+int compareParcels(const void *a, const void *b) {
+    struct Parcel *parcelA = (struct Parcel *)a;
+    struct Parcel *parcelB = (struct Parcel *)b;
+
+    return strcmp(parcelA->date, parcelB->date);
+}
 
 int main() {
-    FILE *file = fopen("Delivery_list.csv", "r");
+    FILE *file;
+    char line[MAX_LINE_SIZE];
+
+    // 파일 열기
+    file = fopen("Delivery_list.csv", "r");
+
+    // 파일 열기에 실패한 경우
     if (file == NULL) {
-        perror("Error opening file");
+        perror("파일을 열 수 없습니다.");
         return 1;
     }
 
-    // 정적 배열을 사용하여 데이터 저장
-    struct Parcel parcels[50];
-    int count = 0;
+    struct Parcel parcels[MAX_PARCELS];
+    int parcelCount = 0;
 
-    // 파일에서 최대 50개의 데이터 읽어오기
-    while (count < 50 && fscanf(file, "%s", parcels[count].code) == 1) {
-        // 데이터를 파싱하여 구조체 멤버에 저장
-        parcels[count].year = extractYear(parcels[count].code);
-        parcels[count].date = extractDate(parcels[count].code);
-        parcels[count].departureRegion = extractDepartureRegion(parcels[count].code);
-        parcels[count].arrivalRegion = extractArrivalRegion(parcels[count].code);
+    // 파일에서 한 줄씩 읽어와서 출력
+    while (fgets(line, sizeof(line), file) != NULL && parcelCount < MAX_PARCELS) {
+        // 함수 호출하여 알파벳 나오기 전과 나온 후로 분리
+                char beforeAlpha[MAX_LINE_SIZE];
+        char afterAlpha[MAX_LINE_SIZE];
+        split(line, beforeAlpha, afterAlpha);
 
-        count++;
+        // 결과를 구조체에 저장
+        strncpy(parcels[parcelCount].date, beforeAlpha, 8);
+        parcels[parcelCount].date[8] = '\0'; // null 종료 문자 추가
+        strncpy(parcels[parcelCount].regionNumbers, beforeAlpha + 8, REGION_CODE_SIZE * 2);
+        parcels[parcelCount].regionNumbers[REGION_CODE_SIZE * 2] = '\0'; // null 종료 문자 추가
+        strncpy(parcels[parcelCount].status, afterAlpha, MAX_LINE_SIZE);
+
+        parcelCount++;
     }
-
-    fclose(file);
 
     // 사용자로부터 정렬 기준 입력 받기
     printf("Choose sorting criteria:\n");
-    printf("1. Year\n");
-    printf("2. Date\n");
-    printf("3. Departure Region\n");
-    printf("4. Arrival Region\n");
+    printf("1. Date\n");
+    printf("2. Departure Region\n");
+    printf("3. Arrival Region\n");
 
     int choice;
     scanf("%d", &choice);
 
-    CompareFunction compareFunction;
-
-    // 선택에 따라 비교 함수 지정
+    // 선택에 따라 정렬 기준 설정
     switch (choice) {
         case 1:
-            compareFunction = compareByYear;
+            qsort(parcels, parcelCount, sizeof(struct Parcel), compareParcels);
             break;
-        case 2:
-            compareFunction = compareByDate;
-            break;
-        case 3:
-            compareFunction = compareByDepartureRegion;
-            break;
-        case 4:
-            compareFunction = compareByArrivalRegion;
-            break;
+        // 추가적인 정렬 기준이 필요한 경우 여기에 추가
         default:
             printf("Invalid choice\n");
+            fclose(file);
             return 1;
     }
 
-    // 정렬
-    quickSort(parcels, 0, count - 1, compareFunction);
-
     // 정렬된 데이터 출력
     printf("\nSorted Parcels:\n");
-    printSortedParcels(parcels, count);
+    for (int i = 0; i < parcelCount; i++) {
+        char from[MAX_LINE_SIZE];
+        char to[MAX_LINE_SIZE];
+
+        convertNumberToString(parcels[i].regionNumbers, from);
+        convertNumberToString(parcels[i].regionNumbers + REGION_CODE_SIZE, to);
+
+        printf("Date: %s\n", parcels[i].date);
+        printf("Departure Region: %s\n", from);
+        printf("Arrival Region: %s\n", to);
+        printf("Delivery Status: %s\n", parcels[i].status);
+        printf("\n");
+    }
+
+    // 파일 닫기
+    fclose(file);
 
     return 0;
 }
 
-// 여기서부터 아래로 partition 함수와 다른 함수들의 정의가 있습니다.
-
-// 비교 함수들
-int compareByYear(struct Parcel a, struct Parcel b) {
-    return a.year - b.year;
-}
-
-int compareByDate(struct Parcel a, struct Parcel b) {
-    return a.date - b.date;
-}
-
-int compareByDepartureRegion(struct Parcel a, struct Parcel b) {
-    return a.departureRegion - b.departureRegion;
-}
-
-int compareByArrivalRegion(struct Parcel a, struct Parcel b) {
-    return a.arrivalRegion - b.arrivalRegion;
-}
-
-void quickSort(struct Parcel arr[], int low, int high, CompareFunction compareFunction) {
-    if (low < high) {
-        int pi = partition(arr, low, high, compareFunction);
-
-        quickSort(arr, low, pi - 1, compareFunction);
-        quickSort(arr, pi + 1, high, compareFunction);
-    }
-}
-
-int partition(struct Parcel arr[], int low, int high, CompareFunction compareFunction) {
-    struct Parcel pivot = arr[high];
-    int i = low - 1;
-
-    for (int j = low; j < high; j++) {
-        if (compareFunction(arr[j], pivot) < 0) {
-            i++;
-            struct Parcel temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
-    }
-
-    struct Parcel temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
-
-    return i + 1;
-}
-
-void sortParcels(struct Parcel parcels[], int count, CompareFunction compareFunction) {
-    quickSort(parcels, 0, count - 1, compareFunction);
-}
-
-void printSortedParcels(struct Parcel parcels[], int count) {
-    for (int i = 0; i < count; i++) {
-        printf("%s\n", parcels[i].code);
-    }
-}
