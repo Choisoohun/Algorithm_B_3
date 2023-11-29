2e77ffd4c83d58cf34c167554483c67d5c9bd8de
Choisoohun <72447176+Choisoohun@users.noreply.github.com>
2023-11-29 21:04:22 +0900

    구조체를 사용하여 전체적인 코드 수정
    출력 내용은 같으나 csv파일의 각 줄을 구조체로 정의하고
    구조체 내에서 날짜, 출발지, 목적지, 택배현황 값을 부여해
    파싱

--- Algorithm_B_3.c
+++ Algorithm_B_3.c
@@ -1,114 +1,94 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
-#define MAX_LINE_SIZE 100 // 크기 조정
-#define REGION_CODE_SIZE 3
+// 구조체 정의
+typedef struct {
+    char date[9];        // 8글자의 날짜 + NULL 문자
+    char from[4];   // 최대 3글자의 출발지 + NULL 문자
+    char to[4]; // 최대 3글자의 도착지 + NULL 문자
+    char situation[4];      // 3글자의 택배현황 + NULL 문자
+} Delivery_code;
 
-//알파벳이 나오기 전과 후를 분리하는 함수 split
-void split(const char *input, char *beforeAlpha, char *afterAlpha) {
-    int i, len = strlen(input);
-
-    // 초기화
-    memset(beforeAlpha, 0, len);
-    memset(afterAlpha, 0, len);
-
-    // 알파벳이 처음 나오는 위치 찾기
-    for (i = 0; i < len; i++) {
-        if (isalpha(input[i])) {
-            break;
-        }
-    }
-
-    // 알파벳이 나오기 전과 나온 후로 분리
-    strncpy(beforeAlpha, input, i);
-    strcpy(afterAlpha, input + i);
-}
-
-// 숫자를 문자열로 변환하는 함수
-const char* convertNumberToString(const char *number, char *result) {
-    // 숫자에 따른 지역 이름 매핑
-    if (strncmp(number, "02", REGION_CODE_SIZE) == 0) {
-        strcpy(result, "서울");
-    } else if (strncmp(number, "051", REGION_CODE_SIZE) == 0) {
-        strcpy(result, "부산");
-    } else if (strncmp(number, "053", REGION_CODE_SIZE) == 0) {
-        strcpy(result, "대구");
-    } else if (strncmp(number, "032", REGION_CODE_SIZE) == 0) {
-        strcpy(result, "인천");
-    } else if (strncmp(number, "062", REGION_CODE_SIZE) == 0) {
-        strcpy(result, "광주");
-    } else if (strncmp(number, "042", REGION_CODE_SIZE) == 0) {
-        strcpy(result, "대전");
-    } else if (strncmp(number, "052", REGION_CODE_SIZE) == 0) {
-        strcpy(result, "울산");
-    } else if (strncmp(number, "044", REGION_CODE_SIZE) == 0) {
-        strcpy(result, "세종");
-    } else if (strncmp(number, "031", REGION_CODE_SIZE) == 0) {
-        strcpy(result, "경기");
-    } else if (strncmp(number, "033", REGION_CODE_SIZE) == 0) {
-        strcpy(result, "강원");
-    } else if (strncmp(number, "043", REGION_CODE_SIZE) == 0) {
-        strcpy(result, "충북");
-    } else if (strncmp(number, "041", REGION_CODE_SIZE) == 0) {
-        strcpy(result, "충남");
-    } else if (strncmp(number, "063", REGION_CODE_SIZE) == 0) {
-        strcpy(result, "전북");
-    } else if (strncmp(number, "061", REGION_CODE_SIZE) == 0) {
-        strcpy(result, "전남");
-    } else if (strncmp(number, "054", REGION_CODE_SIZE) == 0) {
-        strcpy(result, "경북");
-    } else if (strncmp(number, "055", REGION_CODE_SIZE) == 0) {
-        strcpy(result, "경남");
-    } else if (strncmp(number, "064", REGION_CODE_SIZE) == 0) {
-        strcpy(result, "제주");
-    }
-
-    return result;
+// 지역번호를 지역 이름으로 변환하는 함수
+const char* convert(const char* code) {
+    if (strcmp(code, "02") == 0) return "서울";
+    if (strcmp(code, "031") == 0) return "경기";
+    if (strcmp(code, "032") == 0) return "인천";
+    if (strcmp(code, "033") == 0) return "강원";
+    if (strcmp(code, "041") == 0) return "충남";
+    if (strcmp(code, "042") == 0) return "대전";
+    if (strcmp(code, "043") == 0) return "충북";
+    if (strcmp(code, "044") == 0) return "세종";
+    if (strcmp(code, "051") == 0) return "부산";
+    if (strcmp(code, "052") == 0) return "울산";
+    if (strcmp(code, "053") == 0) return "대구";
+    if (strcmp(code, "054") == 0) return "경북";
+    if (strcmp(code, "055") == 0) return "경남";
+    if (strcmp(code, "061") == 0) return "전남";
+    if (strcmp(code, "062") == 0) return "광주";
+    if (strcmp(code, "063") == 0) return "전북";
+    if (strcmp(code, "064") == 0) return "제주";
+    return "알 수 없음";
 }
 
 int main() {
-    FILE *file;
-    char line[MAX_LINE_SIZE];
-    char date[9]; // 날짜 부분 크기 조정
-    char regionNumbers[REGION_CODE_SIZE * 2 + 1]; // 두 지역 코드 부분 크기 조정
-    char status[MAX_LINE_SIZE];
+    // 파일 경로
+    const char *file_path = "Delivery_list.csv";
 
     // 파일 열기
-    file = fopen("Delivery_list.csv", "r");
-
-    // 파일 열기에 실패한 경우
+    FILE *file = fopen(file_path, "r");
     if (file == NULL) {
-        perror("파일을 열 수 없습니다.");
+        fprintf(stderr, "파일 열기 실패: %s\n", file_path);
         return 1;
     }
 
-    // 파일에서 한 줄씩 읽어와서 출력
+    // 한 줄씩 읽어오기
+    char line[20];
     while (fgets(line, sizeof(line), file) != NULL) {
-        // 함수 호출하여 알파벳 나오기 전과 나온 후로 분리
-        char beforeAlpha[MAX_LINE_SIZE];
-        char afterAlpha[MAX_LINE_SIZE];
-        split(line, beforeAlpha, afterAlpha);
+        // 줄 끝의 개행 문자 제거
+        line[strcspn(line, "\n")] = 0;
 
-        // 결과를 변수에 저장
-        strncpy(date, beforeAlpha, 8);
-        date[8] = '\0'; // null 종료 문자 추가
-        strncpy(regionNumbers, beforeAlpha + 8, REGION_CODE_SIZE * 2);
-        regionNumbers[REGION_CODE_SIZE * 2] = '\0'; // null 종료 문자 추가
-        strncpy(status, afterAlpha, MAX_LINE_SIZE);
+        // 구조체 인스턴스 생성
+        Delivery_code delivery;
 
-        // 각 지역 번호를 문자열로 변환하여 출력
-        char from[MAX_LINE_SIZE];
-        char to[MAX_LINE_SIZE];
+        // line문자열의 처음 8글자를 날짜로 추출
+        strncpy(delivery.date, line, 8);
+        delivery.date[8] = '\0'; // NULL 문자 추가
 
-        convertNumberToString(regionNumbers, from);
-        convertNumberToString(regionNumbers + REGION_CODE_SIZE, to);
+        // 택배 현황 추출
+        int line_len = strlen(line);    
+        strncpy(delivery.situation, line + line_len - 3, 3);    // line + line_len은 문자열의 끝을 가리킴
+        delivery.situation[3] = '\0';
 
-        // 결과 출력
-        printf("날짜: %s\n", date);
-        printf("출발지: %s\n", from);
-        printf("도착지: %s\n", to);
-        printf("택배현황: %s\n", status);
+        // 출발지 추출 (서울인 경우 '02', 그렇지 않은 경우 3글자)
+        if (line[8] == '0' && line[9] == '2') {
+            strncpy(delivery.from, line + 8, 2);
+            delivery.from[2] = '\0';
+        } else {
+            strncpy(delivery.from, line + 8, 3);
+            delivery.from[3] = '\0';
+        }
+
+        // 도착지 추출
+        int toStartIndex = (delivery.from[1] == '2' && strlen(delivery.from) == 2) ? 10 : 11;
+        if (line[toStartIndex] == '0' && line[toStartIndex + 1] == '2') {
+            // 도착지가 서울인 경우
+            strncpy(delivery.to, line + toStartIndex, 2);
+            delivery.to[2] = '\0';
+        } else {
+            // 도착지가 서울이 아닌 경우
+            strncpy(delivery.to, line + toStartIndex, 3);
+            delivery.to[3] = '\0';
+        }
+
+        // 지역번호를 지역 이름으로 변환
+        const char* fromName = convert(delivery.from);
+        const char* toName = convert(delivery.to);
+
+        // 구조체 내용 출력
+        printf("날짜: %s, 출발지: %s, 도착지: %s, 택배현황: %s\n",
+               delivery.date, fromName, toName, delivery.situation);
     }
 
     // 파일 닫기
@@ -116,5 +96,3 @@ int main() {
 
     return 0;
 }
-
-
